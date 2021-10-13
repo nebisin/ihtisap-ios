46c9adf93d3dab7c7ddd6acc0c3237516cdd6a4d
nebisin <massive00@windowslive.com>
2021-10-13 16:08:29 +0300

    Initial Commit

---
+++ ihtisap-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ihtisap-ios
+//
+//  Created by Salim Bozok on 13.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ihtisap-ios/ihtisap_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ihtisap_iosApp.swift
+//  ihtisap-ios
+//
+//  Created by Salim Bozok on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ihtisap_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
